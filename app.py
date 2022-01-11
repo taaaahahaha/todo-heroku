5c4f7bec2de5787a26d4b2cae055531cea0b71c0
Taaha Multani <taaha.m@somaiya.edu>
2022-01-11 19:37:33 +0530

    Used Mongodb as Database

--- app.py
+++ app.py
@@ -1,53 +1,65 @@
+# © Taaha Multani @ https://github.com/taaaahahaha
+
+# Imports for WebApp
 from flask import Flask, render_template, request, redirect
-from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
-app = Flask(__name__,template_folder='template')
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+# Imports for MongoDb Database
+import pymongo
+from pymongo import MongoClient
 
-class Todo(db.Model):
-    sno = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(200), nullable=False)
-    desc = db.Column(db.String(500), nullable=False)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+app = Flask(__name__,template_folder='template',static_folder='static')
 
-    def __repr__(self) -> str:
-        return f"{self.sno} - {self.title}"
+# Temporary Cluster
+cluster = MongoClient("mongodb+srv://taaham:123@cluster0.imuxc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+db = cluster["Todo"]
+collection = db["Data"]
 
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
     if request.method=='POST':
         title = request.form['title']
         desc = request.form['desc']
-        todo = Todo(title=title, desc=desc)
-        db.session.add(todo)
-        db.session.commit()
+
+        count = collection.count()
+
+        while True:
+            try:
+                collection.insert_one(
+                        {
+                            "_id":count+1,
+                            "title":title,
+                            "desc":desc
+                        }
+                    )
+                
+                break
+            except:
+                count+=1
+
         
-    allTodo = Todo.query.all() 
-    return render_template('index.html', allTodo=allTodo)
+        
+    data_obj = collection.find({})
+    return render_template('index.html', allTodo=data_obj, len=collection.count())
 
 @app.route('/update/<int:sno>', methods=['GET', 'POST'])
 def update(sno):
     if request.method=='POST':
         title = request.form['title']
         desc = request.form['desc']
-        todo = Todo.query.filter_by(sno=sno).first()
-        todo.title = title
-        todo.desc = desc
-        db.session.add(todo)
-        db.session.commit()
+        todo = collection.update_one({"_id":sno},{"$set":{"title":title,"desc":desc}})
+        
         return redirect("/")
         
-    todo = Todo.query.filter_by(sno=sno).first()
+    todo = collection.find_one({"_id":sno})
     return render_template('update.html', todo=todo)
 
 @app.route('/delete/<int:sno>')
 def delete(sno):
-    todo = Todo.query.filter_by(sno=sno).first()
-    db.session.delete(todo)
-    db.session.commit()
+    
+    print(type(sno))
+    collection.delete_one({"_id":sno})
+
     return redirect("/")
 
 @app.route("/about")
